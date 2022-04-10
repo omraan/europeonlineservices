d3ab70c43e3742341cbb20ec1c01f25a7c71a257
eos_omraan <omraan.timmerarends@europeonlineservices.com>
2022-04-10 23:16:23 +0200

    temporary solution for checklogin

--- app/code/Eos/FrontPortal/Controller/Howitworks/Index.php
+++ app/code/Eos/FrontPortal/Controller/Howitworks/Index.php
@@ -3,7 +3,6 @@
 namespace Eos\FrontPortal\Controller\Howitworks;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\Request\Http;
 use Magento\Framework\View\Result\PageFactory;
 
 // Composition implements an action interface. Common interfaces to implement:
@@ -16,26 +15,16 @@ class Index implements HttpGetActionInterface
     /** @var PageFactory */
     private $pageFactory;
 
-
-    /** @var Http */
-    protected $_request;
-
     // Instantiating the Context object is no longer required
     public function __construct(
-        PageFactory $pageFactory,
-        Http $request
-
+        PageFactory $pageFactory
     ) {
         // Calling parent::__construct() is also no longer needed
         $this->pageFactory = $pageFactory;
-        $this->_request = $request;
     }
 
     public function execute()
     {
-        echo '<pre>';
-        var_dump(($this->_request->getPathInfo()));
-        die();
         return $this->pageFactory->create();
     }
 }
