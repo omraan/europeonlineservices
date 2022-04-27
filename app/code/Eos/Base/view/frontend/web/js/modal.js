f496bfa78db4c8c5a94926657b77397fcc6a3b44
eos_omraan <omraan.timmerarends@europeonlineservices.com>
2022-04-27 09:01:29 +0200

    Created Modal.js for standardized pop-up

---
+++ app/code/Eos/Base/view/frontend/web/js/modal.js
@@ -0,0 +1,33 @@
+define([
+    'jquery',
+    "Eos_Base/js/bootstrap.bundle.min",
+    'mage/url'
+], function ($,bootstrap,urlBuilder) {
+
+    return function (ID, title, body, url, post, btnText) {
+        $('.modal').remove();
+
+        var myUrl = urlBuilder.build(url);
+
+        var modal = '<div class="modal fade" id="' + ID +'" tabindex="-1" role="dialog" aria-labelledby="' + ID +'Title" aria-hidden="true">\n' +
+            '    <div class="modal-dialog modal-dialog-centered" role="document">\n' +
+            '        <div class="modal-content">\n' +
+            '            <div class="modal-header">\n' +
+            '                <h5 class="modal-title">' + title +'</h5>\n' +
+            '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
+            '                    <span aria-hidden="true">&times;</span>\n' +
+            '                </button>\n' +
+            '            </div>\n' +
+            '            <div class="modal-body">\n' + body + '</div>\n' +
+            '            <div class="modal-footer">\n' +
+            '                <a href="'+ myUrl + '/' + post +' " type="button" class="btn btn-primary">' + btnText + '</a>\n' +
+            '                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '</div>\n';
+
+        $('body').append(modal);
+    }
+
+});
