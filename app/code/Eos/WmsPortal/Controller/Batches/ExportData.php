137c71421241fb81e8c7a234b3b37907cafe355b
Omraan Timmerarends <omraan.timmerarends@europeonlineservices.com>
2023-06-28 21:13:49 +0200

    changed to chinese address street

--- app/code/Eos/WmsPortal/Controller/Batches/ExportData.php
+++ app/code/Eos/WmsPortal/Controller/Batches/ExportData.php
@@ -215,7 +215,7 @@ class ExportData extends \Magento\Framework\App\Action\Action
                 $itemData[] = $address->getData('firstname') . " " . ($address->getData('middlename') ? $address->getData('middlename') . " " : "") . $address->getData('lastname');
                 //$itemData[] = str_replace(";" , " ", explode("\n", $address->getData('street'))[1]);
                 //$itemData[] = str_replace(";" , " ", $address->getData('street'));
-                $itemData[] = $address->getCustomAttribute('chinese_address_street');
+                $itemData[] = $address->getData('chinese_address_street');
                 $itemData[] = '';
                 $itemData[] = $address->getData('postcode');
                 $itemData[] = $chineseAddress['city_en'];
